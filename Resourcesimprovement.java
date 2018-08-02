051e3fbaa5fd178390bc2bd83c3ffec8c14ab204
NIKHIL KANT PANDEY <35057758+nikhilkantpandey@users.noreply.github.com>
2018-08-02 16:32:48 +0530

    TRY WITH RESOURCES IMPROVEMENT

---
+++ Resourcesimprovement.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+public class Resourcesimprovement {
+	public static void main(String[] args) throws IOException {
+	      System.out.println(readData("test"));
+	   } 
+	   static String readData(String message) throws IOException {
+	      Reader inputString = new StringReader(message);
+	      BufferedReader br = new BufferedReader(inputString);
+	      try (br) {
+	         return br.readLine();
+	      }
+	   }
+}
